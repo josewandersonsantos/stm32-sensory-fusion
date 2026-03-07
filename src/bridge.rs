8fcb89bbb1ec13347fc16f184f025a24ef2ed284
josewandersonsantos <wandersonjose75@gmail.com>
2026-03-06 23:17:18 -0300

    Added methods to build package sensors data

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,7 +1,10 @@
-use crate::utils;
+use core::u16;
+
+use crate::{checksum, utils};
 
 const PAYLOAD_SIZE_MAX: usize = 256;
 const SOF: u8 = 0x7E;
+const HEADER_SIZE:usize = core::mem::size_of::<FrameHeader>();
 static mut FRAME_ID: u8 = 1;
 
 #[repr(u8)]
@@ -47,10 +50,17 @@ pub fn get_header(tp: FrameType, len: u16) -> FrameHeader
 
 pub fn build_frame(tp: FrameType, data: &[u8]) -> FrameTx
 {
-    let len = data.len();
-
+    let len = 
+    if data.len() > PAYLOAD_SIZE_MAX
+    {
+        data.len()
+    }
+    else
+    {
+        PAYLOAD_SIZE_MAX
+    };
+    
     let header = get_header(tp, len as u16);
-
     let mut frame = FrameTx
     {
         header,
@@ -58,11 +68,11 @@ pub fn build_frame(tp: FrameType, data: &[u8]) -> FrameTx
         crc: 0
     };
 
-    // copia payload
+    // copy payload
     frame.payload[..len].copy_from_slice(data);
-
-    // calcula CRC
-    frame.crc = utils::get_crc16(&frame.payload[..len], frame.payload.len() as u16);
+    // get CRC16
+    let bytes = utils::as_bytes(&frame);
+    frame.crc = checksum::get_crc16(bytes, (data.len() + HEADER_SIZE) as u16);
 
     frame
 }
@@ -75,4 +85,45 @@ pub fn get_gps_data(data: &[u8]) -> FrameTx
 pub fn get_package_cfg(data: &[u8]) -> FrameTx
 {
     build_frame(FrameType::FR_TYPE_CFG, data)
+}
+
+pub fn get_package_acc_date(x: f32, y: f32, z: f32) -> FrameTx
+{
+    let mut data:[u8; 12] = [0u8; 12];
+    
+    data[0..4].copy_from_slice(&x.to_le_bytes());
+    data[4..8].copy_from_slice(&y.to_le_bytes());
+    data[8..12].copy_from_slice(&z.to_le_bytes());
+
+    build_frame(FrameType::FR_TYPE_ACC_DATA, &data)
+}
+
+pub fn get_package_gyr_date(x: f32, y: f32, z: f32) -> FrameTx
+{
+    let mut data:[u8; 12] = [0u8; 12];
+    
+    data[0..4].copy_from_slice(&x.to_le_bytes());
+    data[4..8].copy_from_slice(&y.to_le_bytes());
+    data[8..12].copy_from_slice(&z.to_le_bytes());
+
+    build_frame(FrameType::FR_TYPE_GYR_DATA, &data)
+}
+
+pub fn get_package_coords(lat: f32, lon: f32) -> FrameTx
+{
+    let mut data:[u8; 8] = [0u8; 8];
+    
+    data[0..4].copy_from_slice(&lat.to_le_bytes());
+    data[4..8].copy_from_slice(&lon.to_le_bytes());
+
+    build_frame(FrameType::FR_TYPE_COORDS, &data)
+}
+
+pub fn verify_package(mut frame: FrameTx) -> u8
+{
+    let crc_rx = frame.crc;
+    let bytes = utils::as_bytes(&frame);
+    let crc_cl = checksum::get_crc16(&bytes, (frame.header.size + HEADER_SIZE as u16));
+    
+    (crc_cl == crc_rx) as u8
 }
