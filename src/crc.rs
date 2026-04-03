237b2b70e456a9d98eb9414e4c8b3d448cce0a3a
josewandersonsantos <wandersonjose75@gmail.com>
2026-04-03 00:26:03 -0300

    Added crc.rs

---
+++ src/crc.rs
@@ -0,0 +1,33 @@
+use crate::{mcu, rcc, utils};
+
+pub fn init()
+{
+    rcc::ahb::enable(rcc::ahb::AHBPeripheral::Crc);
+}
+
+pub fn reset()
+{
+    let crc_cr = mcu::CRC_CR as *mut u32;
+    unsafe
+    {
+        utils::write_register(crc_cr, 1); // RESET bit
+    }
+}
+
+pub fn calc(data: &[u32]) -> u32
+{
+    let crc_dr = mcu::CRC_DR as *mut u32;
+
+    for &word in data
+    {
+        unsafe
+        {
+            utils::write_register(crc_dr, word);
+        }
+    }
+
+    unsafe
+    {
+        utils::read_register(crc_dr)
+    }
+}
